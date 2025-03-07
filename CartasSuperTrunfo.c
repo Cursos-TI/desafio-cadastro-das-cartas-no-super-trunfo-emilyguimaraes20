3631b0edb85095db3a0da27853b2a5623e76ddaa
emilyguimaraes20 <emilly19f@outlook.com>
2025-03-07 00:58:09 +0000

    logica super CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,60 +1,50 @@
 #include <stdio.h>
 
-//variaveis 1 card
-int main() {
+int main(){
+    int Numerodepontosturisticos,populaçao,resultado;
+    char tipocomparaçao;
 
-char estado(Goias);
-char codigo(4);
-char nomecity(Goiania);
-int  pontotur, populaçao;
-float area, pib;
+    // Inicio do jogo 
+    printf("Bem-vindo ao jogo Maior,menor ou igual!\n");
+    printf("Voce deve escolher uma carta e o tipo de comparaaço.\n");
+    printf("M.Maior\n");
+    printf("N.Mneor\n");
+    printf("T.igual\n");
 
+    printf("Escolha a comparaçao:");
+    scanf("%c", &TipoComparaçao);
 
+    printf("Digite sua carta");
+    scanf("%d", numero jogador);
 
-//variaveis 2 card
-char estado(ceara);
-char codigo(7);
-char nomecity(fortaleza);
-int pontotur, populaçao;
+    //exibir numero de pontos turisticos 
 
+    printf("O numero de pontos turisticos e: 5d\n", numerodepontosturisticos);
+    
+    switch (tipocomparaçao)
+    {
+    case M:
+       /*code*/
+       break;
+  case N:
+       /*code*/
+       break;
+  case T:
+       /*code*/
+default
+ printf("Voce escolheu a opçao igual!\n");
+ resultado = Numerodepontosturisticos == populaçao ? 1 : 0;
+ break;
+    }
+default:
+printf(" opçao de jogo invalida\n");
+break;
 
-// cadastro 1 card 
-printf("** bem vindo ao jogo super trunfo dos estados!!***\n");
-printf("vamos digitar os dados da primeira carta? vamos la!!\n").
-printf("-----CARTA 1-----\n");
-printf("Digite uma letra de 'A' a 'H'\n");
-scanf("%c", &Goias);
-Printf("Digite um numero de 01 a 04: \n");
-scanf("%s", &codigo);
-printf("Goiania: \n");
-scanf("%s", &goiania);
-//fgets(goiania, 7, stdin);
-//scanf ("%7[^\n]", &goiania);
-printf("Agora o numero de habitantes da cidade:\n");
-scanf("%d", &populaçao);
-printf("Qual o tamanho da area da cidade?: \n");
-scanf("%d", &area );
-printf("Agora vamos digitar o numero de pontos turisticos: \n");
-scanf("%d" &turismo);
-
-// cadastro 2 card 
-printf("Bem vindo ao jogo super trunfo dos estados!!***\n");
-printf("Vamos digitar os dados da primeira carta? Bora la!!\n");
-printf("-----CARTA2----\n");
-printf("Digite uma letra de 'A' a 'H' \n");
-scanf("%c" &ceara);
-printf("Digite um numero de 01 a 07:\n");
-scanf("%d",&codigo);
-printf("fortaleza: \n");
-scanf("%s", &fortaleza);
-//fgets(fortaleza, 7, stdin);
-//scanf(%7[^\n] , fortaleza);
-printf("Agora o numero de habitantes da cidade:\n ");
-scanf("%d",&populaçao);
-printf("Qual o tamnho da area da cidade?: \n");
-scanf("%d", & area);
-printf("Agora vamos digitar os pontos turisticos:\n");
-scanf("%d", &turismo);
+}
+ if (resultado == 1)
+ {
+    printf ("Voce venceu!")
+ }
 
     return 0;
 }
